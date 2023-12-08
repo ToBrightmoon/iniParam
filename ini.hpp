f6c2a94672b4743cdfd11ad9b67bc78bd8d37094
chengzhao <1870035544@qq.com>
2023-12-08 16:04:55 +0800

    添加项目文件。

---
+++ ini.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Windows.h>
+#include <locale>
+#include <codecvt>
+
+template <class T>
+T GetParam(std::string Root, std::string key, std::string ini_path);
+
+template <>
+int GetParam(std::string root, std::string key, std::string ini_path)
+{
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+    std::wstring w_root = converter.from_bytes(root);
+    std::wstring w_key = converter.from_bytes(key);
+    std::wstring w_path = converter.from_bytes(ini_path);
+    return GetPrivateProfileInt(w_root.c_str(), w_key.c_str(), 10, w_path.c_str());
+}
+
+template <>
+std::string GetParam(std::string root, std::string key, std::string ini_path)
+{
+    wchar_t buffer[1024];
+    std::wstring msg;
+
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+
+    std::wstring w_root = converter.from_bytes(root);
+    std::wstring w_key = converter.from_bytes(key);
+    std::wstring w_path = converter.from_bytes(ini_path);
+
+    GetPrivateProfileString(w_root.c_str(), w_key.c_str(), L"", buffer, sizeof(buffer) / sizeof(wchar_t), w_path.c_str());
+    msg = buffer;
+    return converter.to_bytes(msg);
+}
---
+++ paramTest.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+#include "ini.hpp"
+
+int main()
+{
+
+    std::string path = "C:\\Project\\Cpp\\paramTest\\param.ini";
+    int age = GetParam<int>("Setting","age",path);
+    
+    std::string str = GetParam<std::string>("Param","word",path);
+
+    std::cout << age << "\n";
+    std::cout << str << "\n";
+    return 0;
+}
+
+
